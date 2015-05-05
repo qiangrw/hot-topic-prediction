7fb935e67f1261558cc5528ba3a821528635d212
Runwei Qiang <qiangrw@gmail.com>
2015-05-05 23:36:25 +0800

    add run.sh for joining

---
+++ topic/run.sh
@@ -0,0 +1,3 @@
+#!sh
+perl join_document_topic.pl acl01-10-1.scv.csv  acl01-10-1-document-topic-distributuions.csv
+perl join_document_topic.pl acl02-11-1.scv.csv  acl02-11-1-document-topic-distributuions.csv
