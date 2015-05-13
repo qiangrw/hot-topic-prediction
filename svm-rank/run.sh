27381d803ae1362a8b1706dea5383b3733f17eb9
Runwei Qiang <qiangrw@gmail.com>
2015-05-13 22:20:37 +0800

    update feature, add time feature

--- svm-rank/run.sh
+++ svm-rank/run.sh
@@ -1,8 +1,9 @@
 #!sh
 
-train=../fea-gen/fea/data2011.txt
-test=../fea-gen/fea/data2012.txt
-model=model.txt
-prediction=prediction/prediction.txt
+feano=3
+train=../fea-gen/fea/data2011.fea$feano
+test=../fea-gen/fea/data2012.fea$feano
+model=fea${feano}.model
+prediction=prediction/fea${fea}.prediction
 ./svm_rank_learn -c 20.0 $train $model
 ./svm_rank_classify $test $model $prediction
