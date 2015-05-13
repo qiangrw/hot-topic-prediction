befe26d767d119ed33d954ee065364df6109c454
Runwei Qiang <qiangrw@gmail.com>
2015-05-13 22:25:29 +0800

    support feature no script

--- svm-rank/run.sh
+++ svm-rank/run.sh
@@ -1,9 +1,9 @@
 #!sh
 
-feano=3
+feano=$1
 train=../fea-gen/fea/data2011.fea$feano
 test=../fea-gen/fea/data2012.fea$feano
 model=fea${feano}.model
-prediction=prediction/fea${fea}.prediction
+prediction=prediction/fea${feano}.prediction
 ./svm_rank_learn -c 20.0 $train $model
 ./svm_rank_classify $test $model $prediction
